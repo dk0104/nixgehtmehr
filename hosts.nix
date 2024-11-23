180bb44103fd92dfb0f322c3696abca867ace73e
Denis Keksel <Denis Keksel>
2024-11-23 14:10:11 +0400

    nvidia drivier + sway

--- hosts.nix
+++ hosts.nix
@@ -6,7 +6,7 @@
     platform      = "x86_64-linux";
     stateVersion  = "24.11";
     isWorkstation = true;
-    wm            = "hyprland";
+    wm            = "sway";
    };
 
    schaufel = {
---
+++ system/machine/schuhbox/modules/hardware/graphics-card/default.nix
@@ -0,0 +1,26 @@
+{ pkgs , ...}:
+{
+  hardware.opengl = {
+      enable = true;
+  };
+
+  services.xserver.videoDrivers = [
+     "nvidia"
+  ];
+  
+  hardware.graphics = {
+      enable = true;
+  };
+
+   hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+
+}
+
